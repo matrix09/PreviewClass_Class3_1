c39d4f36b6c55fa1e8f250b4cf92a1844df30e0c
Erric <mateixzxf@icloud.com>
2017-10-15 10:42:41 +0800

    添加体验课工程

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+
+    Animator am;
+
+    void Start()
+    {
+        am = GetComponent<Animator>();
+    }
+
+    float speed = 0f;
+    public void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float he = Input.GetAxis("Horizontal");
+
+        float ve = Input.GetAxis("Vertical");
+
+
+        Quaternion dir = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+
+        Vector3 right = dir * Vector3.right;
+
+        Vector3 forward = dir * Vector3.forward;
+
+        Vector3 mov = ve * forward + he * right;
+
+        speed = 0f;
+
+        if (mov != Vector3.zero)
+        {
+            transform.forward = mov;
+            transform.forward = Vector3.Lerp(transform.forward, mov, 5 * Time.deltaTime);
+            transform.Translate(transform.forward * 5f * Time.deltaTime, Space.World);
+            speed = 1f;
+        }
+
+        am.SetFloat("Speed", speed);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Box"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+
+    public float fmoveSpeed;
+
+    public Vector3 OffSet;
+
+    public Vector3 Dir;
+
+    public Transform player;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = player.position + OffSet;
+
+        Vector3 dir = Dir;
+
+        transform.position = Vector3.Lerp(transform.position, pos, 10 * Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(dir), 10 * Time.deltaTime);
+
+
+
+	}
+}
